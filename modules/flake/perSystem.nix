312e0df621f050bf00c3367e7a5fd35d00253aaf
Arturo <29128253+aig77@users.noreply.github.com>
2026-09-21 19:46:24 -0400

    fix(deploy): fixes call to deploy binary to use correct bin name

--- modules/flake/perSystem.nix
+++ modules/flake/perSystem.nix
@@ -8,7 +8,7 @@
 
     apps.deploy = {
       type = "app";
-      program = "${pkgs.deploy-rs}/bin/deploy-rs";
+      program = "${pkgs.deploy-rs}/bin/deploy";
       meta.description = "Deploy NixOS hosts with deploy-rs";
     };
 
@@ -16,9 +16,13 @@
     # failure and rolls back any node already deployed in this run.
     apps.deploy-servers = {
       type = "app";
-      program = "${pkgs.writeShellScriptBin "deploy-servers" ''
-        exec ${pkgs.deploy-rs}/bin/deploy-rs --targets ".#ed" ".#jet" "$@"
-      ''}/bin/deploy-servers";
+      program = "${pkgs.writeShellApplication {
+        name = "deploy-servers";
+        runtimeInputs = [pkgs.deploy-rs];
+        text = ''
+          exec deploy --targets ".#ed" ".#jet" "$@"
+        '';
+      }}/bin/deploy-servers";
       meta.description = "Deploy all server hosts with deploy-rs";
     };
   };
