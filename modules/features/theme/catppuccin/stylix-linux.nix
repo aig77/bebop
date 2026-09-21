f000e4877fbbde6ae0da0c076136dc970299149d
Arturo <29128253+aig77@users.noreply.github.com>
2026-09-21 17:56:09 -0400

    fix(theme): prebuild catppuccin cursors, drop inkscape build, much faster rebuild times now

--- modules/features/theme/catppuccin/stylix-linux.nix
+++ modules/features/theme/catppuccin/stylix-linux.nix
@@ -6,7 +6,31 @@
   inherit (config.flake.meta.owner) username;
   hm = config.flake.modules.homeManager;
 in {
-  flake.modules.nixos.stylix-catppuccin = {pkgs, ...}: {
+  flake.modules.nixos.stylix-catppuccin = {pkgs, ...}: let
+    # prebuilds catppuccin cursor to avoid building inkscape from source
+    cursor = pkgs.stdenvNoCC.mkDerivation {
+      pname = "catppuccin-cursors";
+      version = "2.0.0";
+      src = pkgs.fetchurl {
+        url = "https://github.com/catppuccin/cursors/releases/download/v2.0.0/catppuccin-mocha-dark-cursors.zip";
+        hash = "sha256-pNl2SRvbGxMRst6IMnytPxxmwtnaiW4MVjYqZgyAJYU=";
+      };
+      nativeBuildInputs = [pkgs.unzip];
+      installPhase = ''
+        runHook preInstall
+        unzip "$src"
+        install -dm755 "$out/share/icons"
+        mv catppuccin-mocha-dark-cursors "$out/share/icons/"
+        runHook postInstall
+      '';
+      meta = {
+        description = "Catppuccin Mocha dark cursor theme (prebuilt)";
+        homepage = "https://github.com/catppuccin/cursors";
+        license = pkgs.lib.licenses.gpl2;
+        platforms = pkgs.lib.platforms.linux;
+      };
+    };
+  in {
     # Scoped to desktop only to avoid breaking server hosts that lack stylix options
     imports = [inputs.stylix.nixosModules.stylix];
 
@@ -36,7 +60,7 @@ in {
 
       cursor = {
         name = "catppuccin-mocha-dark-cursors";
-        package = pkgs.catppuccin-cursors.mochaDark;
+        package = cursor;
         size = 24;
       };
 
